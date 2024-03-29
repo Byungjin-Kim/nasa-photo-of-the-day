9f8ee37952250517073d58f2a0b99e1fb232c326
Byungjin-Kim <composerjins@gmail.com>
2024-03-28 23:54:20 -0500

    envfile add

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "./App.css";
 
 import Nasaphoto from "./Components/Nasaphoto";
 
-
-
 function App() {
   const [data, setData] = useState();
 
-  useEffect (() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_NASA_API_KEY;
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
       .then(res => {
+        // console.log(res.data);
         setData(res.data);
       })
       .catch(err => console.error(err))
